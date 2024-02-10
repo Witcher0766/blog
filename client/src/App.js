05c58e427eee58ba30ce3a4f7963eb8c05080d0c
Witcher0766 <100211783+Witcher0766@users.noreply.github.com>
2024-02-10 11:06:28 +0530

    10feb work

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Register from './pages/loginpage/Register';
 import { UserContextProvider } from './context/UserContext';
 import Create from './pages/create/Create';
 import Postpage from './pages/postpage/Postpage';
+import Editpost from './pages/editpost/Editpost';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
     <Route path="/register" element={<Register/>}/>
     <Route path="/create" element={<Create/>}/>
     <Route path="/post/:id" element={<Postpage/>}/>
+    <Route path='/edit/:id' element={<Editpost/>}/>
     </Route>
  </Routes>
  </UserContextProvider>
