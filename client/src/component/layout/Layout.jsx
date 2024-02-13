80f3aaa012a4d5da34d0500bf0400de8d000827a
Witcher0766 <100211783+Witcher0766@users.noreply.github.com>
2024-02-13 17:01:08 +0530

    13Feb

--- client/src/component/layout/Layout.jsx
+++ client/src/component/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../header/Header'
 import { Outlet } from 'react-router-dom'
+import Box from '../box/Box'
 
 const Layout = () => {
   return (
     <>
         <main>
             <Header/>
+            <Box/>
             <Outlet/>
         </main>
     </>
