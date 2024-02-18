d8ebab783911905e5103870be120ec9e76d5bb83
Witcher0766 <100211783+Witcher0766@users.noreply.github.com>
2024-02-18 16:10:57 +0530

    18feb asdfa

--- client/src/component/box/Box.jsx
+++ client/src/component/box/Box.jsx
@@ -7,11 +7,11 @@ const Box = () => {
 
 
   return (
-    <div className=' h-60 flex flex-col text-left  mb-8 justify-center px-10'>
-<div className='flex flex-col justify-center gap-6  w-2/3'>
-<h1 className='text-5xl font-semibold '>Blog with <span className='text-sky-400'>the best.</span> </h1>
-    <p>More bloggers and independent creators choose WordPress than any other blogging tool. Tap into intuitive, flexible tools that put writers, bloggers, and creators first.</p>
-    <Link to="/create"  className={styles["btn-box"]}>Start a Blog</Link>
+    <div className='flex flex-col text-center p-5  mb-8 justify-center md:p-10 md:text-left'>
+<div className='flex flex-col justify-center gap-6'>
+<h1 className='text-6xl font-semibold md:text-6xl lg:text-7xl'>Blog with <span className='text-sky-400'>the best.</span> </h1>
+    <p className='md:text-lg lg:text-lg'>More bloggers and independent creators choose WordPress than any other blogging tool. Tap into intuitive, flexible tools that put writers, bloggers, and creators first.</p>
+    <div><Link to="/create"  className={styles["btn-box"]}>Start a Blog</Link></div>
 </div>
     </div>
   )
